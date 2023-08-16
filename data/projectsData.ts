d998c2f60329259c6aa62831d7171152fa412cf3
TONY ANTONY <tonyantony300@gmail.com>
2023-08-17 01:19:58 +0530

    first commit

---
+++ data/projectsData.ts
@@ -0,0 +1,69 @@
+const projectsData = [
+  {
+    title: 'Huddle',
+    description: 'Landing page project with styled React components',
+    imgSrc: '/static/images/projects/huddle.png',
+    tech: ['Javascript', 'ReactJs', 'HTML'],
+    githuburl: 'https://github.com/tonyantony300/huddlee',
+    href: 'https://smartpage300.netlify.app/',
+  },
+  {
+    title: 'Cost Estimator',
+    description: 'A static cost estimator implementaion with vanilla CSS',
+    imgSrc: '/static/images/projects/cost-estimator.png',
+    tech: ['HTML', 'CSS'],
+    githuburl: 'https://github.com/tonyantony300/crispy-winner',
+    href: 'https://lively-lokum-9ac19b.netlify.app/',
+  },
+
+  {
+    title: 'Blog post',
+    description: 'A static blog page done with just CSS',
+    imgSrc: '/static/images/projects/blog-post.png',
+    tech: ['HTML', 'CSS'],
+    githuburl: 'https://github.com/tonyantony300/crispy-winner',
+    href: 'https://rococo-quokka-1e981c.netlify.app/',
+  },
+  {
+    title: 'Forkify',
+    description: 'Pure MVC Food recipie search and display application',
+    imgSrc: '/static/images/projects/forkify.png',
+    tech: ['HTML', 'CSS', 'JS', 'API'],
+    githuburl: 'https://github.com/tonyantony300/forkify',
+    href: 'https://marvelous-horse-1a5099.netlify.app',
+  },
+  {
+    title: 'Workout Tracker',
+    description: 'activity tracking app with vue 3 (with composition api), supabase & tailwind css',
+    imgSrc: '/static/images/projects/workout.png',
+    tech: ['Vue', 'supabase', 'tailwind'],
+    githuburl: 'https://github.com/tonyantony300/workout',
+    href: 'https://bucolic-lokum-8329ac.netlify.app/',
+  },
+  {
+    title: 'Travel Website',
+    description: 'A Dynamic home page for travel web application',
+    imgSrc: '/static/images/projects/travel.png',
+    tech: ['ReactJS', 'HTML', 'CSS'],
+    githuburl: 'https://github.com/tonyantony300/travel',
+    href: 'https://mywebsite300.netlify.app',
+  },
+  {
+    title: 'Artist Portfolio',
+    description: 'A static porfolio page done with just CSS',
+    imgSrc: '/static/images/projects/artist-portfolio.png',
+    tech: ['HTML', 'CSS'],
+    githuburl: 'https://github.com/tonyantony300/crispy-winner',
+    href: 'https://dulcet-tartufo-c12503.netlify.app/',
+  },
+  {
+    title: 'Weather widget',
+    description: 'A widget to fetch weather data from AccuweatherApi',
+    imgSrc: '/static/images/projects/weather.png',
+    tech: ['ReactJS', 'HTML', 'CSS'],
+    githuburl: 'https://brilliant-taffy-7d9560.netlify.app/',
+    href: 'https://brilliant-taffy-7d9560.netlify.app/',
+  },
+]
+
+export default projectsData
