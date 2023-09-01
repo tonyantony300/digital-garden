032f61d29e4b2a4d8bb77b0cb5dc1d2fcc0ed759
TONY ANTONY <tonyantony300@gmail.com>
2023-09-01 13:54:44 +0530

    added certifications to projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,15 +1,55 @@
 const projectsData = [
   {
+    title: 'Modern React with Redux Certification',
+    description: 'Recieved after completing 54 hour course work and projects',
+    imgSrc: '/static/images/projects/react-proof.jpg',
+    tech: ['ReactJS', 'Redux'],
+    githuburl: 'https://github.com/tonyantony300/',
+    href: 'https://www.udemy.com/certificate/UC-ebe6e57e-1d04-4732-ad00-e2f8418ed56c/',
+  },
+  {
+    title: 'Javascript DSA Certification',
+    description: 'Received from freecode camp after completing course',
+    imgSrc: '/static/images/projects/algo.png',
+    tech: ['Javascript'],
+    githuburl: 'https://github.com/tonyantony300/',
+    href: 'https://www.freecodecamp.org/certification/tony_antny/javascript-algorithms-and-data-structures',
+  },
+  {
     title: 'Chat App',
-    description: 'Chat interface with React and redux store, using react-chat-kit lib',
+    description: 'Interactive Chat interface with React and redux store, using react-chat-kit lib',
     imgSrc: '/static/images/projects/chat.png',
     tech: ['ReactJS', 'Redux', 'CSS'],
     githuburl: 'https://github.com/tonyantony300/crispy-guacamole',
     href: 'https://crispy-guacamole-seven.vercel.app',
   },
   {
+    title: 'Forkify',
+    description: 'Interactive Pure MVC Food recipie search and display application',
+    imgSrc: '/static/images/projects/forkify.png',
+    tech: ['HTML', 'CSS', 'JS', 'API'],
+    githuburl: 'https://github.com/tonyantony300/forkify',
+    href: 'https://marvelous-horse-1a5099.netlify.app',
+  },
+  {
+    title: 'Workout Tracker',
+    description: 'Interactive activity tracking app with vue 3, supabase & tailwind css',
+    imgSrc: '/static/images/projects/workout.png',
+    tech: ['Vue', 'supabase', 'tailwind'],
+    githuburl: 'https://github.com/tonyantony300/workout',
+    href: 'https://bucolic-lokum-8329ac.netlify.app/',
+  },
+  {
+    title: 'Weather widget',
+    description: 'An Interactive widget to fetch weather data from AccuweatherApi',
+    imgSrc: '/static/images/projects/weather.png',
+    tech: ['ReactJS', 'HTML', 'CSS'],
+    githuburl: 'https://brilliant-taffy-7d9560.netlify.app/',
+    href: 'https://brilliant-taffy-7d9560.netlify.app/',
+  },
+  {
     title: 'Huddle',
-    description: 'Landing page project with styled React components',
+    description: 'Static Landing page project with styled React components',
     imgSrc: '/static/images/projects/huddle.png',
     tech: ['Javascript', 'ReactJs', 'HTML'],
     githuburl: 'https://github.com/tonyantony300/huddlee',
@@ -23,7 +63,6 @@ const projectsData = [
     githuburl: 'https://github.com/tonyantony300/crispy-winner',
     href: 'https://lively-lokum-9ac19b.netlify.app/',
   },
-
   {
     title: 'Blog post',
     description: 'A static blog page done with just CSS',
@@ -31,22 +70,6 @@ const projectsData = [
     tech: ['HTML', 'CSS'],
     githuburl: 'https://github.com/tonyantony300/crispy-winner',
     href: 'https://rococo-quokka-1e981c.netlify.app/',
-  },
-  {
-    title: 'Forkify',
-    description: 'Pure MVC Food recipie search and display application',
-    imgSrc: '/static/images/projects/forkify.png',
-    tech: ['HTML', 'CSS', 'JS', 'API'],
-    githuburl: 'https://github.com/tonyantony300/forkify',
-    href: 'https://marvelous-horse-1a5099.netlify.app',
-  },
-  {
-    title: 'Workout Tracker',
-    description: 'activity tracking app with vue 3 (with composition api), supabase & tailwind css',
-    imgSrc: '/static/images/projects/workout.png',
-    tech: ['Vue', 'supabase', 'tailwind'],
-    githuburl: 'https://github.com/tonyantony300/workout',
-    href: 'https://bucolic-lokum-8329ac.netlify.app/',
   },
   {
     title: 'Travel Website',
@@ -64,14 +87,6 @@ const projectsData = [
   //   githuburl: 'https://github.com/tonyantony300/crispy-winner',
   //   href: 'https://dulcet-tartufo-c12503.netlify.app/',
   // },
-  {
-    title: 'Weather widget',
-    description: 'A widget to fetch weather data from AccuweatherApi',
-    imgSrc: '/static/images/projects/weather.png',
-    tech: ['ReactJS', 'HTML', 'CSS'],
-    githuburl: 'https://brilliant-taffy-7d9560.netlify.app/',
-    href: 'https://brilliant-taffy-7d9560.netlify.app/',
-  },
 ]
 
 export default projectsData
